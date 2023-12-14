958ca6eb615b4fcd34b3176fc1670ebe8cc13551
Randomduren <safonovmax593@gmail.com>
2023-12-14 15:18:18 +0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+document.getElementById("profile").addEventListener("click", function(){
+    document.getElementById("autorisation").classList.add("open")
+})
+
+document.getElementById("profileClose").addEventListener("click", function(){
+    document.getElementById("autorisation").classList.remove("open")
+})
